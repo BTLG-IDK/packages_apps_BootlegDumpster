1c30b57ca2a0a36af54fd793b5eb971fb49918b4
acras01 <acras1@gmail.com>
2021-11-18 17:51:56 +0530

    Dumpster: Allow toggling combined signal icons [2/2]
    * @jhonboy121: remove preference controller usage and use the default value for switch from sysui resource
    idoybh (YAAP): Good ol' java
    
    * nurkeinneid: Add SystemUI restart dialog
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: idoybh <idoybh2@gmail.com>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Change-Id: I47a1758523e08f4751857334f41258b40f825556

--- src/com/bootleggers/dumpster/fragments/StatusBarSettings.java
+++ src/com/bootleggers/dumpster/fragments/StatusBarSettings.java
@@ -4,6 +4,7 @@ import com.android.internal.logging.nano.MetricsProto;
 
 import android.os.Bundle;
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
@@ -19,6 +20,7 @@ import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.SwitchPreference;
 import android.provider.Settings;
+import android.provider.DeviceConfig;
 import com.android.settings.R;
 
 import java.util.Locale;
@@ -26,6 +28,7 @@ import android.text.TextUtils;
 import android.view.View;
 
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.util.bootleg.BootlegUtils;
 import com.android.settings.Utils;
 import android.util.Log;
 
@@ -38,23 +41,53 @@ import java.util.Collections;
 public class StatusBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String COMBINED_STATUSBAR_ICONS = "show_combined_status_bar_signal_icons";
+    private static final String CONFIG_RESOURCE_NAME = "flag_combined_status_bar_signal_icons";
+    private static final String SYSTEMUI_PACKAGE = "com.android.systemui";
+
+    private SwitchPreference mCombinedIcons;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.bootleg_dumpster_frag_status_bar);
 
+        ContentResolver resolver = getActivity().getContentResolver();
         PreferenceScreen prefSet = getPreferenceScreen();
 
+        mCombinedIcons = (SwitchPreference)
+                findPreference(COMBINED_STATUSBAR_ICONS);
+        Resources sysUIRes = null;
+        boolean def = false;
+        int resId = 0;
+        try {
+            sysUIRes = getActivity().getPackageManager()
+                    .getResourcesForApplication(SYSTEMUI_PACKAGE);
+        } catch (Exception ignored) {
+            // If you don't have system UI you have bigger issues
+        }
+        if (sysUIRes != null) {
+            resId = sysUIRes.getIdentifier(
+                    CONFIG_RESOURCE_NAME, "bool", SYSTEMUI_PACKAGE);
+            if (resId != 0) def = sysUIRes.getBoolean(resId);
+        }
+        boolean enabled = Settings.Secure.getInt(resolver,
+                COMBINED_STATUSBAR_ICONS, def ? 1 : 0) == 1;
+        mCombinedIcons.setChecked(enabled);
+        mCombinedIcons.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-        switch (preference.getKey()) {
-            default:
-                return false;
+        if (preference == mCombinedIcons) {
+            boolean enabled = (boolean) newValue;
+            Settings.Secure.putInt(resolver,
+                    COMBINED_STATUSBAR_ICONS, enabled ? 1 : 0);
+            return true;
         }
+        return false;
     }
 
     @Override
